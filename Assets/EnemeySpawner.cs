b9f580a28568c486e8bef6d977b60b056f09b527
LAPTOP-4UDAE21C\VAMSHI <vamshivujini3108@gamil.com>
2021-08-28 10:45:30 +0530

    done with shooter game

---
+++ Assets/EnemeySpawner.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemeySpawner : MonoBehaviour
+{
+    public GameObject astreiod;
+    public  float repeatTime=3.0f;
+    float time = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SpawnEnemies", 2f, repeatTime);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time = time + Time.deltaTime;
+        if (time > 10.0f)
+        {
+            repeatTime--;
+            time = 0;
+        }
+    }
+
+    private void SpawnEnemies()
+    {
+        transform.position = new Vector3(Random.Range(-7, 7), 10.0f, 0);
+       GameObject enemy= Instantiate(astreiod, transform.position, Quaternion.identity);
+        Rigidbody2D rb = enemy.GetComponent<Rigidbody2D>();
+        rb.velocity = Vector2.down;
+    }
+}
---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed;
+    [SerializeField]
+    float xmin, xmax;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float xInput = Input.GetAxis("Horizontal");
+
+        transform.Translate(new Vector3(xInput, transform.position.y, 0) * speed * Time.deltaTime);
+        transform.position = new Vector3(Mathf.Clamp(transform.position.x, xmin, xmax), 0, 0);
+
+
+
+    }
+}
